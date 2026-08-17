a81f002fd2066b39b4b0595e5001e6597faaa430
James Simmons <simmons.ben.david@gmail.com>
2026-08-17 15:00:54 +0900

    Remove rejected fresh-eyes wrapper

--- app/picasso-seen-with-fresh-eyes/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./desktop-opening.css";
-
-export default function FreshEyesLayout({ children }: { children: React.ReactNode }) {
-  return <div className="freshEyesScope">{children}</div>;
-}
