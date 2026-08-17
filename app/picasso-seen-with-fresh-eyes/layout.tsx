800ebc7484dbf04601b0c9deb38bde4ab9051ef9
James Simmons <simmons.ben.david@gmail.com>
2026-08-17 14:31:26 +0900

    Scope fresh-eyes desktop art direction to article route

---
+++ app/picasso-seen-with-fresh-eyes/layout.tsx
@@ -0,0 +1,5 @@
+import "./desktop-opening.css";
+
+export default function FreshEyesLayout({ children }: { children: React.ReactNode }) {
+  return <div className="freshEyesScope">{children}</div>;
+}
