2e50bd5df5a51c1c7f982854cb862cf1c3a9b284
James Simmons <simmons.ben.david@gmail.com>
2026-08-21 20:59:50 +0900

    Load refined mobile cover CTA styles

--- app/eastokyo/layout.tsx
+++ app/eastokyo/layout.tsx
@@ -13,6 +13,7 @@ import "../styles/asagaya-mobile-final.css";
 import "./eastokyo-asagaya-balance-panels.css";
 import "./eastokyo-holographic-images.css";
 import "./eastokyo-mobile-cover-final.css";
+import "./eastokyo-cover-cta-refine.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.eastokyo.com"),
