5a091cda19be1e5207511b71f07c48af15d6fe1c
James Simmons <simmons.ben.david@gmail.com>
2026-08-24 20:02:16 +0900

    Load article layout guardrails last in the global cascade

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,6 +68,7 @@ import "./eastokyo/eastokyo-japan-skin.css";
 import "./eastokyo/eastokyo-japan-skin-fixes.css";
 import "./eastokyo/eastokyo-hard-edged-palette.css";
 import "./eastokyo/eastokyo-mobile-cover-final.css";
+import "./styles/article-layout-guardrails.css";
 
 const sans = Montserrat({ variable: "--font-bienvivos-sans", subsets: ["latin"], display: "swap" });
 const display = Cormorant_Garamond({ variable: "--font-bienvivos-display", subsets: ["latin"], weight: ["400", "500", "600", "700"], style: ["normal", "italic"], display: "swap" });
