50e5f863c392f1131d71002053f0b6a20eea15ca
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 16:43:35 +0900

    Emergency rollback broken Riviera theme overrides

--- app/nothing-is-just-what-it-is/layout.tsx
+++ app/nothing-is-just-what-it-is/layout.tsx
@@ -1,6 +1,5 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
-import "./riviera-theme.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="riviera-route"><SiteHeader />{children}<SiteFooter /></div>;
+  return <div><SiteHeader />{children}<SiteFooter /></div>;
 }
