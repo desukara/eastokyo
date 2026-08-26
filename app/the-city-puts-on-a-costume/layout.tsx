0622f3e7e3d4619836f82d95c5a39be17b8c96d8
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 17:01:44 +0900

    Apply Asagaya visual cleanup overrides

--- app/the-city-puts-on-a-costume/layout.tsx
+++ app/the-city-puts-on-a-costume/layout.tsx
@@ -1,5 +1,6 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import "./festival-force.css";
+import "./festival-cleanup.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <div className="asagayaFestivalShell"><SiteHeader />{children}<SiteFooter /></div>;
