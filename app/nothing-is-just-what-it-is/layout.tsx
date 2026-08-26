d53a74369e9b343fcdf408ba684b932dd665b7f7
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 16:35:53 +0900

    Scope French Riviera theme to index story

--- app/nothing-is-just-what-it-is/layout.tsx
+++ app/nothing-is-just-what-it-is/layout.tsx
@@ -1,5 +1,6 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import "./riviera-theme.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <><SiteHeader />{children}<SiteFooter /></>;
+  return <div className="riviera-route"><SiteHeader />{children}<SiteFooter /></div>;
 }
