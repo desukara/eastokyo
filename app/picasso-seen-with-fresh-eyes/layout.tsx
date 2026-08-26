178c0b3a3b93c18dcedb532f9dd6d227e832f462
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 13:16:52 +0900

    Load Brazilian Blue Boy desktop theme

--- app/picasso-seen-with-fresh-eyes/layout.tsx
+++ app/picasso-seen-with-fresh-eyes/layout.tsx
@@ -1,5 +1,6 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import "./hero-art-direction.css";
+import "./blueboy-desktop-theme.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <>
