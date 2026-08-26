1a2028fd18f0f3e06a0270ea011a045e70dc4e07
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 16:20:42 +0900

    Scope full festival art direction to Asagaya route

--- app/the-city-puts-on-a-costume/layout.tsx
+++ app/the-city-puts-on-a-costume/layout.tsx
@@ -1,5 +1,6 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import "./festival-force.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <><SiteHeader />{children}<SiteFooter /></>;
+  return <div className="asagayaFestivalShell"><SiteHeader />{children}<SiteFooter /></div>;
 }
