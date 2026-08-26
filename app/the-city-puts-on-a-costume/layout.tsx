ac1672b247ab8f522652382a4c9fdaa214f8627c
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 17:39:11 +0900

    Mount Asagaya share prototype

--- app/the-city-puts-on-a-costume/layout.tsx
+++ app/the-city-puts-on-a-costume/layout.tsx
@@ -1,7 +1,8 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import SharePrototype from "./SharePrototype";
 import "./festival-force.css";
 import "./festival-cleanup.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="asagayaFestivalShell"><SiteHeader />{children}<SiteFooter /></div>;
+  return <div className="asagayaFestivalShell"><SharePrototype /><SiteHeader />{children}<SiteFooter /></div>;
 }
