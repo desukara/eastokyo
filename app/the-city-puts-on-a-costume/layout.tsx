e84bb7f0df4eb542c5bd71562200ab892bccd5c8
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 23:45:29 +0900

    Activate Asagaya mobile viewport guard

--- app/the-city-puts-on-a-costume/layout.tsx
+++ app/the-city-puts-on-a-costume/layout.tsx
@@ -1,10 +1,11 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import ShareRailPreview from "./ShareRailPreview";
 import ReactionPreview from "./ReactionPreview";
+import MobileViewportGuard from "./MobileViewportGuard";
 import "./festival-force.css";
 import "./festival-cleanup.css";
 import "./mobile-layout-fix.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="asagayaFestivalShell"><ShareRailPreview /><ReactionPreview /><SiteHeader />{children}<SiteFooter /></div>;
+  return <div className="asagayaFestivalShell"><MobileViewportGuard /><ShareRailPreview /><ReactionPreview /><SiteHeader />{children}<SiteFooter /></div>;
 }
