45a61f441cde03f842b587912141f16b2376f5f4
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 21:48:16 +0900

    Mount Asagaya Like Love Wow preview

--- app/the-city-puts-on-a-costume/layout.tsx
+++ app/the-city-puts-on-a-costume/layout.tsx
@@ -1,8 +1,9 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import ShareRailPreview from "./ShareRailPreview";
+import ReactionPreview from "./ReactionPreview";
 import "./festival-force.css";
 import "./festival-cleanup.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="asagayaFestivalShell"><ShareRailPreview /><SiteHeader />{children}<SiteFooter /></div>;
+  return <div className="asagayaFestivalShell"><ShareRailPreview /><ReactionPreview /><SiteHeader />{children}<SiteFooter /></div>;
 }
