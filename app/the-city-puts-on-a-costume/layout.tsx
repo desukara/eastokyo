f2a5a7a6404b1fba85629cce84dcb2f006feeb09
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 19:11:14 +0900

    Load Asagaya share preview polish

--- app/the-city-puts-on-a-costume/layout.tsx
+++ app/the-city-puts-on-a-costume/layout.tsx
@@ -2,6 +2,7 @@ import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import SharePrototype from "./SharePrototype";
 import "./festival-force.css";
 import "./festival-cleanup.css";
+import "./share-preview-polish.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <div className="asagayaFestivalShell"><SharePrototype /><SiteHeader />{children}<SiteFooter /></div>;
