9b085d8b730f0c140678be18db219ffa726c3155
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 12:27:11 +0900

    Load cubism desktop editorial repair

--- app/when-one-point-of-view-wasnt-enough/layout.tsx
+++ app/when-one-point-of-view-wasnt-enough/layout.tsx
@@ -1,4 +1,5 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import "./desktop-editorial-repair.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <><SiteHeader />{children}<SiteFooter /></>;
