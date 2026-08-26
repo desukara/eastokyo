14fa55bcec09ef634f4a8deb7ea3e85af8b0c710
James Simmons <simmons.ben.david@gmail.com>
2026-08-26 23:21:40 +0900

    Apply Asagaya mobile containment fix

--- app/the-city-puts-on-a-costume/layout.tsx
+++ app/the-city-puts-on-a-costume/layout.tsx
@@ -3,6 +3,7 @@ import ShareRailPreview from "./ShareRailPreview";
 import ReactionPreview from "./ReactionPreview";
 import "./festival-force.css";
 import "./festival-cleanup.css";
+import "./mobile-layout-fix.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <div className="asagayaFestivalShell"><ShareRailPreview /><ReactionPreview /><SiteHeader />{children}<SiteFooter /></div>;
