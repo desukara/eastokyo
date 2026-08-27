5acbebe7d352a090eba41d3ae5b956d7f25a149a
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 17:37:20 +0900

    Load targeted cubism layout repair

--- app/when-one-point-of-view-wasnt-enough/layout.tsx
+++ app/when-one-point-of-view-wasnt-enough/layout.tsx
@@ -2,6 +2,7 @@ import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import CubismEngagement from "./CubismEngagement";
 import "./desktop-editorial-repair.css";
 import "./editorial-polish.css";
+import "./targeted-layout-repair.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <><SiteHeader />{children}<CubismEngagement /><SiteFooter /></>;
