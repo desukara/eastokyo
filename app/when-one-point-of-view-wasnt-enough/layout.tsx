b9b5b1d50086f593500c2798a38a671ab4cff622
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 22:18:09 +0900

    Add elegant English translation control to cubism article

--- app/when-one-point-of-view-wasnt-enough/layout.tsx
+++ app/when-one-point-of-view-wasnt-enough/layout.tsx
@@ -1,9 +1,10 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import FrenchTranslateLink from "../eastokyo/FrenchTranslateLink";
 import CubismEngagement from "./CubismEngagement";
 import "./desktop-editorial-repair.css";
 import "./editorial-polish.css";
 import "./targeted-layout-repair.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <><SiteHeader />{children}<CubismEngagement /><SiteFooter /></>;
+  return <><SiteHeader /><FrenchTranslateLink />{children}<CubismEngagement /><SiteFooter /></>;
 }
