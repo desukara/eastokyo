47a616974ddc671b781b0569595a344614a6781f
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 22:18:27 +0900

    Add elegant English translation control to index article

--- app/nothing-is-just-what-it-is/layout.tsx
+++ app/nothing-is-just-what-it-is/layout.tsx
@@ -1,6 +1,7 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import FrenchTranslateLink from "../eastokyo/FrenchTranslateLink";
 import IndexEngagement from "./IndexEngagement";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div><SiteHeader />{children}<IndexEngagement /><SiteFooter /></div>;
+  return <div><SiteHeader /><FrenchTranslateLink />{children}<IndexEngagement /><SiteFooter /></div>;
 }
