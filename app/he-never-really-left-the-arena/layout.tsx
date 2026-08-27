5375fcaf12633f50d4cedf9cc712c36a32be7979
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 15:43:41 +0900

    Mount Arena engagement system

--- app/he-never-really-left-the-arena/layout.tsx
+++ app/he-never-really-left-the-arena/layout.tsx
@@ -1,6 +1,7 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import ArenaEngagement from "./ArenaEngagement";
 import "./arena-finale-polish.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="arena-story-theme"><SiteHeader />{children}<SiteFooter /></div>;
+  return <div className="arena-story-theme"><ArenaEngagement /><SiteHeader />{children}<SiteFooter /></div>;
 }
