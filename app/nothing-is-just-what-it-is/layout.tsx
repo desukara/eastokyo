70b82c8924cd081f68be28c32fb7ac34656b778f
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 21:27:26 +0900

    Mark Picasso index article as French

--- app/nothing-is-just-what-it-is/layout.tsx
+++ app/nothing-is-just-what-it-is/layout.tsx
@@ -2,5 +2,5 @@ import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import IndexEngagement from "./IndexEngagement";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div><SiteHeader />{children}<IndexEngagement /><SiteFooter /></div>;
+  return <div lang="fr-FR"><SiteHeader />{children}<IndexEngagement /><SiteFooter /></div>;
 }
