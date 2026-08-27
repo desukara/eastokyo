c766f5d35c4497488f312b3cd7f015a9565b5f4a
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 22:18:50 +0900

    Add elegant English translation control to arena article

--- app/he-never-really-left-the-arena/layout.tsx
+++ app/he-never-really-left-the-arena/layout.tsx
@@ -1,7 +1,8 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import FrenchTranslateLink from "../eastokyo/FrenchTranslateLink";
 import ArenaEngagement from "./ArenaEngagement";
 import "./arena-finale-polish.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="arena-story-theme"><ArenaEngagement /><SiteHeader />{children}<SiteFooter /></div>;
+  return <div className="arena-story-theme"><ArenaEngagement /><SiteHeader /><FrenchTranslateLink />{children}<SiteFooter /></div>;
 }
