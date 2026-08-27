088a3d6731770759b7cae3f010854f655ae1fde7
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 21:28:01 +0900

    Mark arena article as French

--- app/he-never-really-left-the-arena/layout.tsx
+++ app/he-never-really-left-the-arena/layout.tsx
@@ -3,5 +3,5 @@ import ArenaEngagement from "./ArenaEngagement";
 import "./arena-finale-polish.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="arena-story-theme"><ArenaEngagement /><SiteHeader />{children}<SiteFooter /></div>;
+  return <div className="arena-story-theme" lang="fr-FR"><ArenaEngagement /><SiteHeader />{children}<SiteFooter /></div>;
 }
