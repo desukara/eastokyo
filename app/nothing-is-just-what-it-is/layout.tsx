510101becb20d52a3011f30b540549d995a4fb4f
James Simmons <simmons.ben.david@gmail.com>
2026-08-27 16:23:29 +0900

    Mount Picasso Index engagement bars

--- app/nothing-is-just-what-it-is/layout.tsx
+++ app/nothing-is-just-what-it-is/layout.tsx
@@ -1,5 +1,6 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
+import IndexEngagement from "./IndexEngagement";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div><SiteHeader />{children}<SiteFooter /></div>;
+  return <div><SiteHeader />{children}<IndexEngagement /><SiteFooter /></div>;
 }
