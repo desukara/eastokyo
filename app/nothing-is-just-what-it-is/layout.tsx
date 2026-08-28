918e7f4574d338fd4eb92699a734779719fa8e18
James Simmons <simmons.ben.david@gmail.com>
2026-08-29 08:35:07 +0900

    Load Picasso index audit fixes

--- app/nothing-is-just-what-it-is/layout.tsx
+++ app/nothing-is-just-what-it-is/layout.tsx
@@ -1,7 +1,8 @@
 import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import FrenchTranslateLink from "../eastokyo/FrenchTranslateLink";
 import IndexEngagement from "./IndexEngagement";
+import "./audit-fixes.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div><SiteHeader /><FrenchTranslateLink />{children}<IndexEngagement /><SiteFooter /></div>;
+  return <div className="indexStoryShell"><SiteHeader /><FrenchTranslateLink />{children}<IndexEngagement /><SiteFooter /></div>;
 }
