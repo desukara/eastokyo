b6ebb9992ad01d66b1c691133fd83555828eb48a
James Simmons <simmons.ben.david@gmail.com>
2026-08-29 07:42:01 +0900

    Load Cubism responsive fixes

--- app/when-one-point-of-view-wasnt-enough/layout.tsx
+++ app/when-one-point-of-view-wasnt-enough/layout.tsx
@@ -4,6 +4,7 @@ import CubismEngagement from "./CubismEngagement";
 import "./desktop-editorial-repair.css";
 import "./editorial-polish.css";
 import "./targeted-layout-repair.css";
+import "./responsive-fixes.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <><SiteHeader /><FrenchTranslateLink />{children}<CubismEngagement /><SiteFooter /></>;
