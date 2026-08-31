662ee3f2867fadb4d3ffe9d0059b799f2028f7fe
James Simmons <simmons.ben.david@gmail.com>
2026-09-01 08:40:53 +0900

    Load Arena share background repair

--- app/he-never-really-left-the-arena/layout.tsx
+++ app/he-never-really-left-the-arena/layout.tsx
@@ -2,6 +2,7 @@ import { SiteFooter, SiteHeader } from "../eastokyo/SiteChrome";
 import FrenchTranslateLink from "../eastokyo/FrenchTranslateLink";
 import ArenaEngagement from "./ArenaEngagement";
 import "./arena-finale-polish.css";
+import "./arena-share-background-fix.css";
 
 export default function StoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <div className="arena-story-theme"><ArenaEngagement /><SiteHeader /><FrenchTranslateLink />{children}<SiteFooter /></div>;
